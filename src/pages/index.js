6f5351728248f3d31482340c18ff82ecb6825cda
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-07 07:28:42 +0530

    add clickable links and styled

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,31 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from 'styled-components'
+
+const BlogLink = styled(Link)`
+text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+margin:bottom:20px;
+color:blue;
+`
+
+
 
 export default ({data}) => (
   <Layout>
     <SEO title="Home" />
     <div>
       <h1>Nishant's Blog</h1>
-      <h4> { data.allMarkdownRemark.totalCount } </h4>
+      <h4> { data.allMarkdownRemark.totalCount } blog yet. </h4>
       {
         data.allMarkdownRemark.edges.map(({node})=> (
           <div key={node.id}> 
-          <span> { node.frontmatter.title } - { node.frontmatter.date } </span>
+          <BlogLink to={ node.fields.slug }>
+          <BlogTitle> { node.frontmatter.title } - { node.frontmatter.date } </BlogTitle>
+          </BlogLink>
           <p> {node.excerpt} </p>
           </div>
         ) )
@@ -27,7 +41,7 @@ export default ({data}) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -37,6 +51,9 @@ export const query = graphql`
             author
             description
             title
+          }
+          fields {
+            slug
           }
           excerpt
         }
