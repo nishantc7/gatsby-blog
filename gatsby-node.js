abf715df02df4b609a77493d7b90faecbde7b144
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-07 07:17:04 +0530

    add templates and configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const {createFilePath} = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 exports.onCreateNode = ({node, getNode, actions}) => {
     const {createNodeField} = actions
     if(node.internal.type === `MarkdownRemark`) {
@@ -12,4 +13,33 @@ exports.onCreateNode = ({node, getNode, actions}) => {
 
             })
     }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    const {createPage} = actions
+    return graphql(`
+    {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+      
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve('./src/templates/blog-post.js'),
+                context: {
+                    slug: node.fields.slug
+                }
+
+            })
+        })
+    })
 }
